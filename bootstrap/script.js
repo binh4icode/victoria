db882dc4005a761d590366611b4779c79bca68a2
Binh <binh4icode@gmail.com>
2026-03-13 12:40:10 -0500

    final

---
+++ bootstrap/script.js
@@ -0,0 +1,11 @@
+let buynowBtn = document.getElementsByClassName('btn')
+
+
+function moveOver(obj) {
+    obj.innerHTML = 'Or else...'
+}
+
+
+function moveOut (obj){
+    obj.innerHTML = "Buy Now"
+}
